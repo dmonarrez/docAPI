a25cf1baf3194d8f4478a83c8e30b55cb4574399
David Monarrez <davidmonarrez3@gmail.com>
2019-03-22 14:24:44 -0700

    working on formatting and populating doctor objecs with proper info from api

--- src/backEnd.js
+++ src/backEnd.js
@@ -1,21 +1,12 @@
-export class DocSearch {
-
-  getAll() {
-      return new Promise(function(resolve, reject) {
-        let request = new XMLHttpRequest();
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors`;
-        request.onload = function() {
-          if (this.status === 200) {
-            resolve(request.response);
-          } else {
-            reject(Error(request.statusText));
-          }
-        }
-        request.open("GET", url, true);
-        request.send();
-      });
-    }
-
+export class Doctor {
+  constructor(firstName, lastName, address, phone, website, newPatients) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.phone = phone;
+    this.website = website;
+    this.newPatients = newPatients;
+  }
 
 }
 
--- src/main.js
+++ src/main.js
@@ -2,32 +2,51 @@ import './css/styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { DocSearch } from './backEnd.js';
+import { Doctor } from './backEnd.js';
 
 
 $(document).ready(function() {
 
-  //const api = new DocSearch;
 
- $('#call').click(function(event) {
-   console.log('submit worked');
+ $('.search').submit(function(event) {
+   // console.log('submit start');
    event.preventDefault();
 
+
+   let docObjsArr = [];
+
+   const searchFirstName = $('#firstNameInput').val();
+
+
    let request = new XMLHttpRequest();
-   let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.apiKey}`
+   let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${searchFirstName}&location=or-portland&user_location=45.512%2C%20122.658&skip=0&limit=10&user_key=${process.env.apiKey}`
 
    request.onreadystatechange = function() {
+     // console.log('request reached');
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
-        console.log(response);
+        console.log(response.data);
+        response.data.forEach(function(doc){
+          const doctor = new Doctor;
+          doctor.firstName = doc.profile.first_name;
+          doctor.lastName = doc.profile.last_name;
+          doctor.address = doc.profile.last_name;
+          doctor.phone = doc.profile.last_name;
+          doctor.website = doc.profile.last_name;
+          doctor.newPatients = doc.profile.last_name;
+          console.log(doc);
+          docObjsArr.push(doctor);
+        });
       } else {
-        console.log('not ready');
+        // console.log('not ready');
       }
     }
 
     request.open("GET", url, true);
     request.send();
-
+    // console.log('open send');
+    // console.log(docObjsArr);
+    // console.log(docObjsArr);
  });
 
 
