d437eae3a0cc4f9662aaf8cf02bc9ba5a7c3d40d
David Monarrez <davidmonarrez3@gmail.com>
2019-03-22 17:09:33 -0700

    program not finished

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { Doctor } from './backEnd.js';
 
 
 $(document).ready(function() {
-
+  //call api for list of conditions on page load
   let conditionsRequest = new XMLHttpRequest();
   let urlConditions = `https://api.betterdoctor.com/2016-03-01/conditions?user_key=516c164ae2d246c222625a09b6daf207`
 
@@ -32,10 +32,12 @@ $(document).ready(function() {
    let docObjsArr = [];
 
    const searchFirstName = $('#firstNameInput').val();
+   const query = $('#conditionInput').val();
+   console.log(query);
 
 
    let request = new XMLHttpRequest();
-   let urlDocs = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${searchFirstName}&location=or-portland&user_location=45.512%2C%20122.658&skip=0&limit=10&user_key=${process.env.apiKey}`
+   let urlDocs = `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=or-portland&user_location=45.512%2C%20122.658&skip=0&limit=10&user_key=${process.env.apiKey}`
 
    request.onreadystatechange = function() {
      // console.log('request reached');
@@ -61,7 +63,7 @@ $(document).ready(function() {
     // console.log(docObjsArr);
 
     // request.send();
-    console.log(docObjsArr);
+    // console.log(docObjsArr);
  });
 
 
